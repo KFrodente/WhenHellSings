c9f35a8b4de52dd46f51a528df28acb5abe9212d
KFrodente <kianfrodente@gmail.com>
2023-11-29 20:06:18 -0700

    bird enemy and trigger boxes work

--- Assets/Kian Folder/Scripts/Enemy/EnemyParent.cs
+++ Assets/Kian Folder/Scripts/Enemy/EnemyParent.cs
@@ -8,7 +8,7 @@ public abstract class EnemyParent : MonoBehaviour
     [SerializeField] protected int health;
     [SerializeField] protected int contactDamage;
     [SerializeField, Tooltip("When the player touches this enemy, how many invincibility time do they get?")] protected float contactITime;
-    [SerializeField] protected float moveSpeed;
+    [SerializeField] public float moveSpeed;
     [SerializeField] protected bool facingRight;
 
     protected Rigidbody2D rb;
